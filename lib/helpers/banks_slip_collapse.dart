7c969b6ed63e200e1c43a4c58da0bdaafd2bf05a
Darkx32 <Darkx32@users.noreply.github.com>
2026-05-30 22:53:42 -0300

    [Style & performance] Better style on home page and better performance when expand bankslips cards

--- lib/helpers/banks_slip_collapse.dart
+++ lib/helpers/banks_slip_collapse.dart
@@ -20,54 +20,74 @@ class BanksSlipCollapse extends StatefulWidget {
   State<StatefulWidget> createState() => _BanksSlipCollapseState();
 }
 
-class _BanksSlipCollapseState extends State<BanksSlipCollapse> {
+class _BanksSlipCollapseState extends State<BanksSlipCollapse> with TickerProviderStateMixin {
   bool _isColapsed = true;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surfaceContainer,
-        border: Border.all(
-          color:  Theme.of(context).colorScheme.onSurface,
-          width: 1.0,
-          style: BorderStyle.solid
-        ),
-        borderRadius: BorderRadius.circular(5)
-      ),
-      padding: EdgeInsets.all(10),
-      margin: EdgeInsets.only(bottom: 10),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Align(
-                  alignment: Alignment.centerLeft
-                )
-              ),
-              Text("${toBeginningOfSentenceCase(widget.month)}/${widget.year}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: IconButton(
-                    visualDensity: VisualDensity.compact,
-                    iconSize: 40,
-                    onPressed: () { setState(() {
-                      _isColapsed = !_isColapsed;
-                    });}, icon: Icon(_isColapsed ? Icons.arrow_right : Icons.arrow_drop_down)
-                  ),
-                )
-              )
-            ],
-          ),
+    final String localeString = Localizations.localeOf(context).toString();
+    final DateFormat formatter = DateFormat.MMMM(localeString);
 
-          if (!_isColapsed)
-            for (var bankSlip in widget.data.reversed) 
-              if (DateFormat.MMMM(Localizations.localeOf(context).toString()).format(bankSlip.value.date) == widget.month)
-                widget.onDraw(bankSlip)
-        ],
-      ),
+    final bankslipsMonth = widget.data.reversed.where((bankslip)  {
+      return formatter.format(bankslip.value.date) == widget.month;
+    });
+
+    return AnimatedSize(
+      alignment: AlignmentGeometry.topCenter,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeIn,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surfaceContainer,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade700,
+              spreadRadius: -5,
+              blurRadius: 10,
+              offset: const Offset(0, 10)
+            )
+          ],
+          border: Border.all(
+            color:  Theme.of(context).colorScheme.onSurface,
+            width: 1.0,
+            style: BorderStyle.solid
+          ),
+          borderRadius: BorderRadius.circular(5)
+        ),
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(bottom: 10),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Expanded(
+                  child: Align(
+                    alignment: Alignment.centerLeft
+                  )
+                ),
+                Text("${toBeginningOfSentenceCase(widget.month)}/${widget.year}", style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      visualDensity: VisualDensity.compact,
+                      iconSize: 40,
+                      onPressed: () { setState(() {
+                        _isColapsed = !_isColapsed;
+                      });}, icon: Icon(_isColapsed ? Icons.arrow_right : Icons.arrow_drop_down)
+                    ),
+                  )
+                )
+              ],
+            ),
+
+            if (!_isColapsed)
+              ...bankslipsMonth.map((bankSlip) {
+                return widget.onDraw(bankSlip);
+              })
+          ],
+        ),
+      )
     );
   }
 }
